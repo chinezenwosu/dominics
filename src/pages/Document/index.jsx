82546327df671fd6fdbf3ca915fc8c5a8078d576
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-20 04:32:54 +0100

    connect socket on client

--- src/pages/Document/index.jsx
+++ src/pages/Document/index.jsx
@@ -1,24 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import ReactQuill from 'react-quill'
+import io from 'socket.io-client'
 import Toolbar, { toolbarModule } from './Toolbar'
+import config from '../../config'
 import 'react-quill/dist/quill.snow.css'
 
 const Document = () => {
-  const [value, setValue] = useState(null)
-  const { id } = useParams()
+  const editorRef = useRef()
+  const [socket, setSocket] = useState(null)
+  const { id: docId } = useParams()
   const modules = { toolbar: toolbarModule }
 
-  const onChange = (content) => {
-    setValue(content)
+  useEffect(() => {
+    const soc = io(config.url.socket)
+    setSocket(soc)
+
+    return () => {
+      soc.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (socket === null) return
+
+    socket.once('load-document', (document) => {
+      editorRef.current.getEditor().setContents(document)
+      editorRef.current.getEditor().enable()
+    })
+
+    socket.emit('get-document', docId)
+  }, [socket, docId])
+
+  useEffect(() => {
+    if (socket === null) return
+  
+    const handler = (delta) => {
+      editorRef.current.getEditor().updateContents(delta)
+    }
+
+    socket.on('receive-document-changes', handler)
+
+    return () => {
+      socket.off('receive-document-changes', handler)
+    }
+  }, [socket])
+
+  const onChange = (_content, delta, source, editor) => {
+    if (socket === null || source !== 'user') return
+
+    socket.emit('send-document-changes', delta)
+    socket.emit('save-document', editor.getContents())
   }
 
   return (
     <>
       <Toolbar />
       <ReactQuill
+        ref={editorRef}
         theme="snow"
-        value={value}
+        readOnly={true}
+        value="Loading document..."
         onChange={onChange}
         modules={modules}
       />
